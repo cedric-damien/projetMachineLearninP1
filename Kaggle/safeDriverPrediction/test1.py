1c7611b4a3e65e2b179f4c5c8c909c7d9ededeaf
cedric-damien <cedric.damien@polytechnique.edu>
2017-11-26 19:37:57 +0100

    [UPD] evaluation

--- Kaggle/safeDriverPrediction/test1.py
+++ Kaggle/safeDriverPrediction/test1.py
@@ -6,12 +6,8 @@ This is a temporary script file.
 """
 import pandas as pd
 import numpy as np
-from sklearn.neighbors import NearestNeighbors
 from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.cross_validation import cross_val_score
-from sklearn.metrics import r2_score, mean_squared_error
 import time
-
 
 np.random.seed(0)
 path_train = '/Users/cedric/Documents/projetMachineLearninP1/Kaggle/safeDriverPrediction/train.csv'
@@ -21,7 +17,7 @@ df_tot = pd.read_csv(path_train)
 df_Data = pd.read_csv(path_test)
 print df_tot[df_tot['id']==1063865]
 
-def ModelGradientboosting(df = df_tot, df_Data = df_Data, sample = 300000, ID = 'id',target='target'):
+def ModelGradientboosting(df = df_tot, df_Data = df_Data, sample = 400000, ID = 'id',target='target'):
     df_train=df.sample(n=sample,frac=None,random_state=0)
     df_test=df[~df[ID].isin(df_train[ID])]
     df_train.set_index(ID, inplace=True)
@@ -32,35 +28,50 @@ def ModelGradientboosting(df = df_tot, df_Data = df_Data, sample = 300000, ID = 
 
     df_Ytest = pd.DataFrame(df_test[target])
     df_Xtest = df_test.drop(target, axis=1)
-    n_estimators = [80]
-    Learning_rate = [ 0.01]
-    max_depth = [4]
-    min_samples_split = [2]
+    loss = ['deviance', 'exponential']
+    n_estimators = [80,500]
+    Learning_rate = [0.01,0.1]
+    max_depth = [2,4,6,8]
+    min_samples_split = [2,300,100]
+    subsample = [1.0,0.7]
+    max_features =['auto', None]
     L = []
     Dict = {}
     Start_Timer = time.clock()
-    for i in Learning_rate:
-        for j in n_estimators: 
-            for k in max_depth:
-                for m in min_samples_split:            
-                    GBest = GradientBoostingClassifier(n_estimators=j,
-                                                       learning_rate=i,
-                                                       max_depth = k,
-                                                       min_samples_split = m,
-                                                       verbose=1).fit(df_Xtrain, df_Ytrain)
+    for ii in loss:
+        for i in Learning_rate:
+            for j in n_estimators: 
+                for k in max_depth:
+                    for m in min_samples_split:   
+                        for n in subsample:
+                            for o in max_features:
+                                GBest = GradientBoostingClassifier(loss = ii,
+                                                                    n_estimators=j,
+                                                                   learning_rate=i,
+                                                                   max_depth = k,
+                                                                   min_samples_split = m,
+                                                                   subsample = n,
+                                                                   max_features = o,
+                                                                   verbose=1).fit(df_Xtrain, df_Ytrain)
+                                
+                                nicolecaca=GBest.predict_proba(df_Xtest)
+                                nicolegroscaca=pd.DataFrame(nicolecaca)
+                                nicolegroscaca.rename(columns={0:'proba0', 1:'target'}, inplace= True)
+                                df_Ytest['proba0Predicted'] = nicolegroscaca['proba0']
+                                df_Ytest['Evaluation'] =(1-df_Ytest['target'])*df_Ytest['proba0Predicted'] - df_Ytest['target']*df_Ytest['proba0Predicted']
+                                Score  = int(np.sum(df_Ytest['Evaluation']))*100.0/(len(df_Ytest)-int(np.sum(df_Ytest['target'])))
+                                
+                                print (Score,'loss=',ii,
+                                             'learningRate=',i,
+                                             'n_estimators=',j,
+                                             'max_depth=',k,
+                                             'min_samples_split=',m,
+                                             'subsample=',n,
+                                             'max_features=',o)                
+                                Dict[(ii,i,j,k, m,n,o)] = Score
+                                print "--- %s seconds --- " %(time.clock() - Start_Timer)
                     
-                    df_Ytest['targetPredicted'] = GBest.predict(df_Xtest)
-                    df_Ytest['Evaluation'] = np.square(df_Ytest['targetPredicted'] - df_Ytest['target'])
-                    Score  = int(np.sum(df_Ytest['Evaluation']))*100.0/len(df_Ytest['Evaluation'])
-                    
-                    print (Score,'learningRate=',i,
-                                 'n_estimators=',j,
-                                 'max_depth=',k,
-                                 'min_samples_split=',m)                
-                    Dict[(i,j,k, m)] = Score
-                    print "--- %s seconds --- " %(time.clock() - Start_Timer)
-            
-    A = min(Dict, key=Dict.get)
+    A = max(Dict, key=Dict.get)
     print Dict[A]
     GTrue = GradientBoostingClassifier(n_estimators=A[1],
                                    learning_rate=A[0],
@@ -74,48 +85,12 @@ def ModelGradientboosting(df = df_tot, df_Data = df_Data, sample = 300000, ID = 
     df_Final = pd.concat([df_result, pd.DataFrame(df_Data.index)], axis =1)
     del df_Final['proba0']
     df_Final.set_index(ID, inplace=True)
-    df_Final.to_csv('/Users/cedric/Documents/projetMachineLearninP1/Kaggle/safeDriverPrediction/result2.csv', sep=',')       
+    df_Final.to_csv('/Users/cedric/Documents/projetMachineLearninP1/Kaggle/safeDriverPrediction/result2arrondi.csv', sep=',')       
     L.append(A)
     L.append(Dict)
     L.append(df_Final)
     print "--- %s seconds --- " %(time.clock() - Start_Timer)
-#   return L
+    return L
 
 
 Result = ModelGradientboosting()
-
-
-
-#   
-#    predictions = []
-#    predictions = pd.DataFrame(predictions)
-#    predictions['Gradient Boosting'] = GBest.predict(df_Xtest)
-#
-
-
-
-
-#    
-#    df_Data.set_index(ID, inplace=True)
-#    A = GBest.predict_proba(df_Data)
-#    df_result = pd.DataFrame(A)
-#    df_result.rename(columns={0:'proba0', 1:'target'}, inplace= True)
-#    df_Final = pd.concat([df_result, pd.DataFrame(df_Data.index)], axis =1)
-#    del df_Final['proba0']
-#    df_Final.set_index(ID, inplace=True)
-##   df_Final.to_csv('/Users/cedric/Documents/projetMachineLearninP1/Kaggle/safeDriverPrediction/result2.csv', sep=',')
-#    
-#    
-##    caca= df_result[df_result['proba1']>0.3]
-#    print predictions.head(50)
-                          
-    
-
-    
-    
- #   scores_predictions = pd.DataFrame()
-"""  scores_predictions['Gradient Boosting'] = pd.Series([mean_squared_error(GBest.predict(df_Xtest), df_Ytest), r2_score(GBest.predict(df_Xtest), df_Ytest)], index=['MSE', 'R2'], name='Gradient Boosting')
-    resultats_GB = pd.DataFrame({'Id':df_test.index, 'proba' : pd.DataFrame(np.expm1(GBest.predict(df_test)))[0]})
-    
-    return resultats_GB
-"""
